7fb370ceea503ee652e4be448f749f2f539e3b1d
Aurelien Joubert <aure.joubert@gmail.com>
2021-01-14 11:06:57 +0100

    🔨 Add data importer tool

---
+++ lib/services/importer.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:the_dead_masked_company.price_comparator/models/item_model.dart';
+import 'package:the_dead_masked_company.price_comparator/models/price_model.dart';
+import 'package:the_dead_masked_company.price_comparator/models/store_model.dart';
+import 'package:the_dead_masked_company.price_comparator/resources/item_repository.dart';
+import 'package:the_dead_masked_company.price_comparator/resources/price_repository.dart';
+import 'package:the_dead_masked_company.price_comparator/resources/store_repository.dart';
+
+/// The Importer widget
+///
+/// This widget is used to import data from JSON file
+/// Currently it's just a dev tool
+class Importer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+  /// Run the importer to read in JSON file and write in Firestore database
+  static Future<void> run(BuildContext context) async {
+    var data = await DefaultAssetBundle.of(context).loadString(
+        'assets/price-comparator-8dea8-p5BcCXgRF9gLwLD0zMsaC0RnbFI2-export.json');
+
+    var jsonResult = json.decode(data) as Map<String, dynamic>;
+
+    for (var name in jsonResult['items_list'] as List<dynamic>) {
+      await ItemRepository.add(ItemModel(name.toString()));
+    }
+    jsonResult.remove('items_list');
+
+    for (var name in jsonResult['stores_list'] as List<dynamic>) {
+      await StoreRepository.add(StoreModel(name.toString()));
+    }
+    jsonResult.remove('stores_list');
+
+    jsonResult.forEach((key, dynamic values) {
+      var item = ItemModel(key.substring(11));
+
+      for (var value in values as List<dynamic>) {
+        var priceValue = json.decode(value.toString()) as Map<String, dynamic>;
+        if (priceValue['price'] != '') {
+          var store = StoreModel(priceValue['store'].toString());
+          var price = PriceModel(item, store,
+              value: priceValue['price'] as num,
+              options: {
+                'isBio': priceValue['isBio'].toString() == 'true',
+                'isCan': priceValue['isCan'].toString() == 'true',
+                'isFreeze': priceValue['isFreeze'].toString() == 'true',
+                'isWrap': priceValue['isWrap'].toString() == 'true',
+                'isUnavailable':
+                    priceValue['isUnavailable'].toString() == 'true',
+              });
+          PriceRepository.add(price);
+        }
+      }
+    });
+  }
+}
--- lib/ui/settings.dart
+++ lib/ui/settings.dart
@@ -4,6 +4,7 @@ import 'package:the_dead_masked_company.price_comparator/resources/price_reposit
 import 'package:the_dead_masked_company.price_comparator/resources/store_repository.dart';
 import 'package:the_dead_masked_company.price_comparator/resources/user_repository.dart';
 import 'package:the_dead_masked_company.price_comparator/services/globals.dart';
+import 'package:the_dead_masked_company.price_comparator/services/importer.dart';
 import 'package:the_dead_masked_company.price_comparator/services/translate.dart';
 
 /// The Settings list widget
@@ -15,28 +16,56 @@ class SettingsList extends StatefulWidget {
 }
 
 class _SettingsListState extends State<SettingsList> {
+  bool loaderVisible = false;
+
   @override
   Widget build(BuildContext context) {
+    var loader = loaderVisible
+        ? Container(
+            margin: const EdgeInsets.only(top: 20.0),
+            child: CircularProgressIndicator())
+        : Container();
+
     return Scaffold(
         appBar: AppBar(title: Text(Translate.translate('Paramètres'))),
         body: Container(
             margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                RaisedButton(
-                  onPressed: () async {
-                    UserRepository.dispose();
-                    PriceRepository.dispose();
-                    ItemRepository.dispose();
-                    StoreRepository.dispose();
-                    Navigator.pop(context);
-                    await Navigator.of(context)
-                        .pushReplacementNamed(Constants.loginScreen);
-                  },
-                  child: Text(Translate.translate('LOGOUT'),
-                      style: TextStyle(fontSize: 20)),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    RaisedButton(
+                      onPressed: () async {
+                        UserRepository.dispose();
+                        PriceRepository.dispose();
+                        ItemRepository.dispose();
+                        StoreRepository.dispose();
+                        Navigator.pop(context);
+                        await Navigator.of(context)
+                            .pushReplacementNamed(Constants.loginScreen);
+                      },
+                      child: Text(Translate.translate('LOGOUT'),
+                          style: TextStyle(fontSize: 20)),
+                    ),
+                    RaisedButton(
+                      onPressed: () async {
+                        setState(() {
+                          loaderVisible = true;
+                        });
+                        await Importer.run(context).then((e) {
+                          setState(() {
+                            loaderVisible = false;
+                          });
+                          Navigator.pop(context);
+                        });
+                      },
+                      child: Text(Translate.translate('IMPORT PREVIOUS DATA'),
+                          style: TextStyle(fontSize: 20)),
+                    ),
+                  ],
                 ),
+                loader
               ],
             )));
   }
